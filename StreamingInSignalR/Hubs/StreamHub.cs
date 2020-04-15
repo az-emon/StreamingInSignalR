f2f613987f530e184121cfaf6cfd68d93f87eb50
Ashraf <az.emon@hotmail.com>
2020-04-15 23:09:44 +0800

    Add project files.

---
+++ StreamingInSignalR/Hubs/StreamHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace StreamingInSignalR.Hubs
+{
+    public class StreamHub :Hub
+    {
+        public ChannelReader<int> DelayCounter(int delay)
+        {
+            var channel = Channel.CreateUnbounded<int>();
+
+            _ = WriteItems(channel.Writer, 1000, delay);
+
+            return channel.Reader;
+        }
+
+        private async Task WriteItems(ChannelWriter<int> writer, int count, int delay)
+        {
+            for (var i = 100; i < count; i = i+100)
+            {
+                await writer.WriteAsync(i);
+                await Task.Delay(delay);
+            }
+
+            writer.TryComplete();
+        }
+    }
+}
